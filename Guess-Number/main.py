5b14fa63b7b2f7f97bc00d5cd4bbed62f2f8983d
Christopher Glenn <topherblair@gmail.com>
2020-10-31 20:26:25 -0600

    Initial commit

---
+++ Guess-Number/main.py
@@ -0,0 +1,4 @@
+# NUMBER GUESSING GAME!!!
+# randomly generate a number between 1 and 100 and let the player
+# try to guess what the number is. Let them know if they are higher
+# or lower than the generated number until they get it right
