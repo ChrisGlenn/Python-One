641b9dfab42fc4797711a8ef86d01a00e093e252
Christopher Glenn <topherblair@gmail.com>
2020-11-01 20:34:40 -0700

    Goodnight

--- Dice-Roller/main.py
+++ Dice-Roller/main.py
@@ -2,3 +2,8 @@
 # the user will input the number of 6-sided dice they want to roll
 # and then the result will be displayed and the user will have the chance
 # to roll the dice again if they so choose
+import random
+diceNumber = 0
+dice = []
+
+print("******* DICE ROLLER *******")
