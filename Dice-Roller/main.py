57f0647e0729ff5676d1ebe6de16a3eda21255e4
Christopher Glenn <topherblair@gmail.com>
2020-11-01 20:03:21 -0700

    Initial Dice-Roller commit

---
+++ Dice-Roller/main.py
@@ -0,0 +1,4 @@
+# DICE ROLLER PROJECT
+# the user will input the number of 6-sided dice they want to roll
+# and then the result will be displayed and the user will have the chance
+# to roll the dice again if they so choose
